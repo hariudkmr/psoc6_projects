e8ae7b94e0c7ce4ab6868d015cdd42b201896e33
Udayakumar Hari (PSS PCS WCS AS PDP SE 3) <Hari.Udayakumar@infineon.com>
2024-06-24 07:08:41 +0100

    removed duplicate sort include in clang-format

--- inc/FreeRTOSConfig.h
+++ inc/FreeRTOSConfig.h
@@ -171,8 +171,8 @@ to exclude the API function. */
 #define INCLUDE_xTaskGetSchedulerState      1
 #define INCLUDE_xTaskGetCurrentTaskHandle   1
 #define INCLUDE_uxTaskGetStackHighWaterMark 0
-#define INCLUDE_xTaskGetIdleTaskHandle      1 
-#define INCLUDE_eTaskGetState               1 
+#define INCLUDE_xTaskGetIdleTaskHandle      1
+#define INCLUDE_eTaskGetState               1
 #define INCLUDE_xEventGroupSetBitFromISR    1
 #define INCLUDE_xTimerPendFunctionCall      1
 #define INCLUDE_xTaskAbortDelay             0
