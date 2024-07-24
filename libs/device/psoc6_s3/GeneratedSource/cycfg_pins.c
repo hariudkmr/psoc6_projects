165198b62d4efaa9fa8effd45b243dfc69929861
Hari Udayakumar <hariudkmr@gmail.com>
2024-07-24 20:14:44 +0530

    build : update make file

--- libs/device/psoc6_s3/GeneratedSource/cycfg_pins.c
+++ libs/device/psoc6_s3/GeneratedSource/cycfg_pins.c
@@ -4,7 +4,7 @@
 * Description:
 * Pin configuration
 * This file was automatically generated and should not be modified.
-* Tools Package 2.4.1.9711
+* Tools Package 2.4.0.5972
 * mtb-pdl-cat1 2.4.1.17937
 * personalities 6.0.0.0
 * udd 3.0.0.2024
@@ -29,11 +29,11 @@
 
 #include "cycfg_pins.h"
 
-const cy_stc_gpio_pin_config_t ioss_0_port_11_pin_1_config = 
+const cy_stc_gpio_pin_config_t LED9_config = 
 {
     .outVal = 1,
     .driveMode = CY_GPIO_DM_ANALOG,
-    .hsiom = ioss_0_port_11_pin_1_HSIOM,
+    .hsiom = LED9_HSIOM,
     .intEdge = CY_GPIO_INTR_DISABLE,
     .intMask = 0UL,
     .vtrip = CY_GPIO_VTRIP_CMOS,
@@ -46,19 +46,19 @@ const cy_stc_gpio_pin_config_t ioss_0_port_11_pin_1_config =
     .vohSel = 0UL,
 };
 #if defined (CY_USING_HAL)
-    const cyhal_resource_inst_t ioss_0_port_11_pin_1_obj = 
+    const cyhal_resource_inst_t LED9_obj = 
     {
         .type = CYHAL_RSC_GPIO,
-        .block_num = ioss_0_port_11_pin_1_PORT_NUM,
-        .channel_num = ioss_0_port_11_pin_1_PIN,
+        .block_num = LED9_PORT_NUM,
+        .channel_num = LED9_PIN,
     };
 #endif //defined (CY_USING_HAL)
 
 
 void init_cycfg_pins(void)
 {
-    Cy_GPIO_Pin_Init(ioss_0_port_11_pin_1_PORT, ioss_0_port_11_pin_1_PIN, &ioss_0_port_11_pin_1_config);
+    Cy_GPIO_Pin_Init(LED9_PORT, LED9_PIN, &LED9_config);
 #if defined (CY_USING_HAL)
-    cyhal_hwmgr_reserve(&ioss_0_port_11_pin_1_obj);
+    cyhal_hwmgr_reserve(&LED9_obj);
 #endif //defined (CY_USING_HAL)
 }
--- libs/device/psoc6_wb/GeneratedSource/cycfg_pins.c
+++ libs/device/psoc6_wb/GeneratedSource/cycfg_pins.c
@@ -4,7 +4,7 @@
 * Description:
 * Pin configuration
 * This file was automatically generated and should not be modified.
-* Tools Package 2.4.1.9711
+* Tools Package 2.4.0.5972
 * mtb-pdl-cat1 2.4.1.17937
 * personalities 6.0.0.0
 * udd 3.0.0.2024
@@ -29,6 +29,30 @@
 
 #include "cycfg_pins.h"
 
+const cy_stc_gpio_pin_config_t USER_config = 
+{
+    .outVal = 1,
+    .driveMode = CY_GPIO_DM_HIGHZ,
+    .hsiom = USER_HSIOM,
+    .intEdge = CY_GPIO_INTR_FALLING,
+    .intMask = 1UL,
+    .vtrip = CY_GPIO_VTRIP_CMOS,
+    .slewRate = CY_GPIO_SLEW_FAST,
+    .driveSel = CY_GPIO_DRIVE_1_2,
+    .vregEn = 0UL,
+    .ibufMode = 0UL,
+    .vtripSel = 0UL,
+    .vrefSel = 0UL,
+    .vohSel = 0UL,
+};
+#if defined (CY_USING_HAL)
+    const cyhal_resource_inst_t USER_obj = 
+    {
+        .type = CYHAL_RSC_GPIO,
+        .block_num = USER_PORT_NUM,
+        .channel_num = USER_PIN,
+    };
+#endif //defined (CY_USING_HAL)
 const cy_stc_gpio_pin_config_t LED9_config = 
 {
     .outVal = 0,
@@ -125,10 +149,63 @@ const cy_stc_gpio_pin_config_t ioss_0_port_5_pin_1_config =
         .channel_num = ioss_0_port_5_pin_1_PIN,
     };
 #endif //defined (CY_USING_HAL)
+const cy_stc_gpio_pin_config_t ioss_0_port_6_pin_6_config = 
+{
+    .outVal = 1,
+    .driveMode = CY_GPIO_DM_PULLUP,
+    .hsiom = ioss_0_port_6_pin_6_HSIOM,
+    .intEdge = CY_GPIO_INTR_DISABLE,
+    .intMask = 0UL,
+    .vtrip = CY_GPIO_VTRIP_CMOS,
+    .slewRate = CY_GPIO_SLEW_FAST,
+    .driveSel = CY_GPIO_DRIVE_1_2,
+    .vregEn = 0UL,
+    .ibufMode = 0UL,
+    .vtripSel = 0UL,
+    .vrefSel = 0UL,
+    .vohSel = 0UL,
+};
+#if defined (CY_USING_HAL)
+    const cyhal_resource_inst_t ioss_0_port_6_pin_6_obj = 
+    {
+        .type = CYHAL_RSC_GPIO,
+        .block_num = ioss_0_port_6_pin_6_PORT_NUM,
+        .channel_num = ioss_0_port_6_pin_6_PIN,
+    };
+#endif //defined (CY_USING_HAL)
+const cy_stc_gpio_pin_config_t ioss_0_port_6_pin_7_config = 
+{
+    .outVal = 1,
+    .driveMode = CY_GPIO_DM_PULLDOWN,
+    .hsiom = ioss_0_port_6_pin_7_HSIOM,
+    .intEdge = CY_GPIO_INTR_DISABLE,
+    .intMask = 0UL,
+    .vtrip = CY_GPIO_VTRIP_CMOS,
+    .slewRate = CY_GPIO_SLEW_FAST,
+    .driveSel = CY_GPIO_DRIVE_1_2,
+    .vregEn = 0UL,
+    .ibufMode = 0UL,
+    .vtripSel = 0UL,
+    .vrefSel = 0UL,
+    .vohSel = 0UL,
+};
+#if defined (CY_USING_HAL)
+    const cyhal_resource_inst_t ioss_0_port_6_pin_7_obj = 
+    {
+        .type = CYHAL_RSC_GPIO,
+        .block_num = ioss_0_port_6_pin_7_PORT_NUM,
+        .channel_num = ioss_0_port_6_pin_7_PIN,
+    };
+#endif //defined (CY_USING_HAL)
 
 
 void init_cycfg_pins(void)
 {
+    Cy_GPIO_Pin_Init(USER_PORT, USER_PIN, &USER_config);
+#if defined (CY_USING_HAL)
+    cyhal_hwmgr_reserve(&USER_obj);
+#endif //defined (CY_USING_HAL)
+
     Cy_GPIO_Pin_Init(LED9_PORT, LED9_PIN, &LED9_config);
 #if defined (CY_USING_HAL)
     cyhal_hwmgr_reserve(&LED9_obj);
@@ -147,5 +224,15 @@ void init_cycfg_pins(void)
     Cy_GPIO_Pin_Init(ioss_0_port_5_pin_1_PORT, ioss_0_port_5_pin_1_PIN, &ioss_0_port_5_pin_1_config);
 #if defined (CY_USING_HAL)
     cyhal_hwmgr_reserve(&ioss_0_port_5_pin_1_obj);
+#endif //defined (CY_USING_HAL)
+
+    Cy_GPIO_Pin_Init(ioss_0_port_6_pin_6_PORT, ioss_0_port_6_pin_6_PIN, &ioss_0_port_6_pin_6_config);
+#if defined (CY_USING_HAL)
+    cyhal_hwmgr_reserve(&ioss_0_port_6_pin_6_obj);
+#endif //defined (CY_USING_HAL)
+
+    Cy_GPIO_Pin_Init(ioss_0_port_6_pin_7_PORT, ioss_0_port_6_pin_7_PIN, &ioss_0_port_6_pin_7_config);
+#if defined (CY_USING_HAL)
+    cyhal_hwmgr_reserve(&ioss_0_port_6_pin_7_obj);
 #endif //defined (CY_USING_HAL)
 }
